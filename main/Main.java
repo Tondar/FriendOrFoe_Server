71eeb2d39054f16279aab767ef1e44cdff862135
Kai Bittner <kai.bittner@t-online.de>
2014-12-03 19:33:59 +0100

    Server fraget die Anzahl der Spieler ab

--- main/Main.java
+++ main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Automatisch generierter Methodenstub
-
+		Scanner userInput = new Scanner(System.in); //Liest die Benutzereingaben 
+		
+		System.out.println("Willkommen!");
+		System.out.println("Geben sie bitte die Anzahl der Spieler ein:");
+		if(userInput.hasNextInt()) {
+			//TODO threadpool aufbauen
+		} else {
+			System.out.println("Bitte ein Zahl eingeben!");
+		}
 	}
 
 }
