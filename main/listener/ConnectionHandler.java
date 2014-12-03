43316e1830f384a3a873ef36b45d3d763a61038f
Kai Bittner <kai.bittner@t-online.de>
2014-12-03 20:31:34 +0100

    ConnectionHandler hinzugefügt

---
+++ main/listener/ConnectionHandler.java
@@ -0,0 +1,11 @@
+package listener;
+
+public class ConnectionHandler implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+}
--- main/main/Main.java
+++ main/main/Main.java
@@ -6,14 +6,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in); //Liest die Benutzereingaben 
+		int numberOfPlayers = 0;
 		
 		System.out.println("Willkommen!");
-		System.out.println("Geben sie bitte die Anzahl der Spieler ein:");
-		if(userInput.hasNextInt()) {
-			//TODO threadpool aufbauen
-		} else {
-			System.out.println("Bitte ein Zahl eingeben!");
+		while (numberOfPlayers == 0 ) {
+			System.out.println("Geben sie bitte die Anzahl der Spieler ein:");
+			if(userInput.hasNextInt()) {
+				numberOfPlayers = userInput.nextInt();
+			} else {
+				System.out.println("Bitte ein Zahl eingeben!");
+				userInput.next();
+			}
 		}
+		
+		//TODO threadpool aufbauen
 	}
 
 }
